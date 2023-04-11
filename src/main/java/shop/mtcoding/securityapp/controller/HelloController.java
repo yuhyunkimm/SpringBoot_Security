09ed14ed70add5ba10634ae6f5e6004d9269e5fd
yuhyunkimm <yuhyunk7@gmail.com>
2023-04-11 10:11:26 +0900

    usercheck 설정

--- src/main/java/shop/mtcoding/securityapp/controller/HelloController.java
+++ src/main/java/shop/mtcoding/securityapp/controller/HelloController.java
@@ -1,11 +1,14 @@
 package shop.mtcoding.securityapp.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
+import shop.mtcoding.securityapp.core.auth.MyUserDetails;
 import shop.mtcoding.securityapp.dto.ResponseDTO;
 import shop.mtcoding.securityapp.dto.UserRequest;
 import shop.mtcoding.securityapp.dto.UserResponse;
@@ -16,6 +19,14 @@ import shop.mtcoding.securityapp.service.UserService;
 public class HelloController {
 
     private final UserService userService;
+
+    // 인증이 필요한 주소를 갓을 때, 인증이 되지 않으니 페이지를 가준다
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> userCheck(@PathVariable Long id, @AuthenticationPrincipal MyUserDetails myUserDetails) {
+        String username = myUserDetails.getUser().getUsername();
+        String role = myUserDetails.getUser().getRole();
+        return ResponseEntity.ok().body(username + " : " + role);
+    }
 
     @GetMapping("/")
     public ResponseEntity<?> hello() {
